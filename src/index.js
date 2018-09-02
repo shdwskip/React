25be8e8409e69bf33e8b426a63bc81f1e94274ac
shdwskip <denev.pavel@gmail.com>
2018-09-02 19:21:37 +0300

    Video search page completed.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const CONFIG = require('../config');
 const API_KEY = CONFIG.API;
 
-YTSearch({key: API_KEY, term: 'iphone'}, function(data) {
-    console.log(data);
-});
+class App extends Component { 
+    constructor(props) {
+        super(props);
 
-const App = () => { 
-    return (
-      <div>
-          <SearchBar />
-      </div>
-    ); 
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
+
+        this.videoSearch('iphone');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term}, (videos) => {
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+             });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        ); 
+    }
 } 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
